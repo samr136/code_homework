987a19902f4537e291043d5c6d9b9d6defedcb42
samr136 <98902137+samr136@users.noreply.github.com>
2022-02-16 15:53:17 -0500

    Add files via upload

--- codehw3.php
+++ codehw3.php
@@ -5,24 +5,40 @@
 </head>
 <body>
 <?php
-$number="0647532699";
+$number="0747532699";
 echo "CHALLENGE: ISBN VALIDATION"."<br>"."<br>";
 echo "Checking ISBN: $number for validity....... "."<br>";
 
+if(validate($number)==true)
+  echo "This is a VALID ISBN"."<br>";
+
+else
+  echo "This is an INVALID ISBN"."<br>";
+
+function validate($number)
+{
 $sum=0;
 $i=10;
 $j=0;
 while($i>0)
-{
+{ 
+if($number[9]=="X")
+$sum+=10;
+
+else  
 $sum+=$i*$number[$j];
 $i--;
 $j++;
 }
 if($sum%11==0)
-  echo "This is a VALID ISBN"."<br>";
+  return true;
 
 else
-  echo "This is an INVALID ISBN"."<br>";
+  return false; 
+}
+
+
+
 ?>
 </body>
 </html>
