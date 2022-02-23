72d40906f77f522c44320a16d4a44483ecbe3d9d
samr136 <98902137+samr136@users.noreply.github.com>
2022-02-23 15:32:45 -0500

    Update codehw5.php

--- codehw5.php
+++ codehw5.php
@@ -3,9 +3,48 @@
 <head>
     <title>CHALLENGE: 3 </title>
 </head>
-<body>
-<?php
+<body><?php
+//Challenge 1 - Book Lists
 
+echo "<h1>"."Displaying books' details and total price"."</h1>";
+$book_list= array(
+array("PHP and MySQL Web Development","Luke Welling","144","Paperback",31.63),
+array("Web Design with HTML, CSS, JavaScript and jQuery","Jon Duckett","135","Paperback",41.23),
+array("PHP Cookbook: Solutions & Examples for PHP Programmers","David Sklar","14","Paperback",40.88),
+array("JavaScript and JQuery: Interactive Front-End Web Development","Jon Duckett","251","Paperback",22.09),
+array("Modern PHP: New Features and Good Practices","Josh Lockhart","7","Paperback",28.49),
+array("Programming PHP","Kevin Tatroe","26","Paperback",28.96));
+
+echo "<table border='6' class='stats' cellspacing='2'>
+  <tr bgcolor='#808080'>
+    <td align='center'>Tile</td>
+    <td align='center'>Name</td>
+    <td align='center'>Number of Pages</td>
+    <td align='center'>Type</td>
+    <td align='center'>Price</td>
+  </tr>";
+$sum=0;
+for($i=0;$i<6;$i++)
+{
+  echo('<tr>');
+  echo('<td style="height:20px;width:800px">' . $book_list[$i][0] . '</td>');
+  echo('<td style="height:20px;width:400px">' . $book_list[$i][1] . '</td>');
+  echo('<td style="height:20px;width:400px">' . $book_list[$i][2] . '</td>');
+  echo('<td style="height:20px;width:150px">' . $book_list[$i][3] . '</td>');
+  echo('<td style="height:20px;width:200px">' . $book_list[$i][4] . '</td>');
+  echo('</tr>');
+  /*echo $book_list[$i][0]." ".$book_list[$i][1]." ".$book_list[$i][2]." ".$book_list[$i][3]." ".$book_list[$i][4]."<br>";*/
+    $sum+=$book_list[$i][4];
+  
+}
+echo "</table>";
+ echo "<h2 style='color:green'>"."Total Price is : $$sum"."</h2>"; 
+?>
+
+
+<?php
+echo "<br>";
+echo "-----------------------------------------------------------------------------------------------------------------------------------------------------"."<br>";
 function coin_toss($coin)
 {
 $i=1;
@@ -39,46 +78,7 @@ coin_toss(4);
 
 ?>
 
-<?php
-//Challenge 1 - Book Lists
-echo "<br>";
-echo "---------------------------------------------------------------------------------------------------------------------------------"."<br>";
-echo "<h1>"."Displaying books' details and total price"."</h1>";
-$book_list= array(
-array("PHP and MySQL Web Development","Luke Welling","144","Paperback",31.63),
-array("Web Design with HTML, CSS, JavaScript and jQuery","Jon Duckett","135","Paperback",41.23),
-array("PHP Cookbook: Solutions & Examples for PHP Programmers","David Sklar","14","Paperback",40.88),
-array("JavaScript and JQuery: Interactive Front-End Web Development","Jon Duckett","251","Paperback",22.09),
-array("Modern PHP: New Features and Good Practices","Josh Lockhart","7","Paperback",28.49),
-array("Programming PHP","Kevin Tatroe","26","Paperback",28.96));
 
-echo "<table border='6' class='stats' cellspacing='2'>
-  <tr bgcolor='#808080'>
-    <td align='center'>Tile</td>
-    <td align='center'>Name</td>
-    <td align='center'>Number of Pages</td>
-    <td align='center'>Type</td>
-    <td align='center'>Price</td>
-  </tr>";
-$sum=0;
-for($i=0;$i<6;$i++)
-{
-  echo('<tr>');
-  echo('<td style="height:20px;width:800px">' . $book_list[$i][0] . '</td>');
-  echo('<td style="height:20px;width:400px">' . $book_list[$i][1] . '</td>');
-  echo('<td style="height:20px;width:400px">' . $book_list[$i][2] . '</td>');
-  echo('<td style="height:20px;width:150px">' . $book_list[$i][3] . '</td>');
-  echo('<td style="height:20px;width:200px">' . $book_list[$i][4] . '</td>');
-  echo('</tr>');
-  /*echo $book_list[$i][0]." ".$book_list[$i][1]." ".$book_list[$i][2]." ".$book_list[$i][3]." ".$book_list[$i][4]."<br>";*/
-    $sum+=$book_list[$i][4];
-  
-}
-echo "<table table border='6' align='center' style='height:20px;width:700px' bordercolor='#FFFF00'>
-  <tr bgcolor='#FFFF00'>
-    <td align='center'> Total Price is : $sum</td>
-  </tr>";
-?>
 
 </body>
 </html>
